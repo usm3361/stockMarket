37397cc8f18b046c2350c614827efcdc0b65b3f3
Uri Miller <usm3361@gmail.com>
2025-12-11 12:25:24 +0200

    added func operateOnStock

---
+++ app.js
@@ -0,0 +1,45 @@
+import {
+  filterStocksByPrice,
+  searchStock,
+  operateOnStock,
+} from "./stockExchange";
+import { printMenu } from "./utils/menuPrint";
+import input from "analiza-sync";
+
+let trade = true;
+while (trade) {
+  let choice = printMenu();
+
+  if (choice === "1") {
+    const identifier = input(
+      "Please enter the ID or name of the stock you are looking for\nPlease enter:   "
+    );
+    searchStock(identifier);
+  } else if (choice === "2") {
+    const givenPrice = parseInt(input("Please enter a stock price\nprice:   "));
+    let above;
+    const aboveOrbelow = input("Do you want above price Y/N:   ");
+    if (aboveOrbelow === "y" || aboveOrbelow === "Y") {
+      above = true;
+    } else if (aboveOrbelow === "n" || aboveOrbelow === "N") {
+      above = false;
+    }
+    filterStocksByPrice(givenPrice, above);
+  } else if (choice === "3") {
+    console.log("1. i want to buy");
+    console.log("2. i want to sell");
+    const operation = input(
+      "Please enter number if you would like to sell or buy:     "
+    );
+    const identifier = input(
+      "Please enter an identifier on which you would like to perform the operation\nidentifier:    "
+    );
+    if (operation === 1) {
+      operateOnStock("buy", identifier);
+    } else {
+      operateOnStock("sell", identifier);
+    }
+  } else if (choice === "4") {
+    trade = false;
+  }
+}
--- utils/stockExchange.js
+++ utils/stockExchange.js
@@ -1,5 +1,6 @@
 import input from "analiza-sync";
 import { stockMarket } from "../data/data";
+import { updateStockCount } from "./updateFunc";
 
 export function searchStock(identifier) {
   return stockMarket.stocks.filter(
@@ -8,13 +9,34 @@ export function searchStock(identifier) {
 }
 
 export function filterStocksByPrice(givenPrice, above) {
-  if (above === true) {
-    return stockMarket.stocks.filter(
+  let arreyStock 
+    if (above === true) {
+    arreyStock = stockMarket.stocks.filter(
       (stock) => stock.currentPrice > givenPrice
     );
-  } else if (above === false) {
-    return stockMarket.stocks.filter(
+  } else (above === false) {
+    arreyStock = stockMarket.stocks.filter(
       (stock) => stock.currentPrice < givenPrice
     );
-  }
 }
+    if (arreyStock.length === 0) {
+        console.log('not found stocks')
+        return arreyStock
+    } arreyStock = []
+    return arreyStock
+}
+
+export function operateOnStock(operation, identifier) {
+    const found = stockMarket.stocks.find(stock => stock.id === identifier || stock.name === identifier)
+    if (operation === 'buy' && found.length > 0) {
+        const amount = input('how many units to buy or sell:\n')
+        found.availableStocks -= amount
+        found.currentPrice
+        
+    } else if (operation === 'sell' && found.length > 0) {
+        const amount = input('how many units to sell or sell:\n')
+        found.availableStocks += amount
+    }
+    
+}
+
